0c360ef2c66a6af1f28b5620e9e6214d52701979
hbk671104 <hbk671104@gmail.com>
2023-06-02 20:13:15 +0800

    run every hour by default

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import cron from 'node-cron'
 import crawler from './crawler.js'
 
-const expression = process.env.CRON_EXPRESSION || '* * * * *'
+const expression = process.env.CRON_EXPRESSION || '0 * * * *'
 
 cron.schedule(expression, async () => {
   await crawler.run([
